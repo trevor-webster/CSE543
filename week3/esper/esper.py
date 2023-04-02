37fcbdab4beaf415d1958ced28667e7d594ec6d5
trevor <trevor>
2023-04-02 22:14:07 +0000

    esper plaintext

--- week3/esper/esper.py
+++ week3/esper/esper.py
@@ -15,16 +15,16 @@ args = parser.parse_args()
 encrypting = True
 
 try:
-    ciphertext = open(args.decrypt, "rb", encoding='utf-8').read()
+    ciphertext = open(args.decrypt, "rb").read()
     try:
-        plaintext = open(args.encrypt, "rb", encoding='utf-8').read()
+        plaintext = open(args.encrypt, "rb").read()
         print("You can't specify both -e and -d")
         exit(1)
     except Exception:
         encrypting = False
 except Exception:
     try:
-        plaintext = open(args.encrypt, "rb", encoding='utf-8').read()
+        plaintext = open(args.encrypt, "rb").read()
     except Exception:
         print("Input file error (did you specify -e or -d?)")
         exit(1)
@@ -35,8 +35,9 @@ def lrot(n, d):
 def rrot(n, d):
     return ((n >> d) ) | ((n << (8 - d)& 0xff))
 
+
+# decrypts only bytes, doesn't apply encoding
 def decrypt(ciphertext, key, keyrotate):
-    ciphertext = bytes(ciphertext)
     plaintext2 = []    
     keyxor2 = []
     for i in range(0,len(key)):
@@ -44,34 +45,29 @@ def decrypt(ciphertext, key, keyrotate):
 
     for i in range(0, len(ciphertext)):
         plaintext2.append(rrot((ciphertext[i] ^ keyxor2[i % len(keyxor2)]), keyrotate))
-
-    # plaintext2 = ''.join(map(lambda a : chr(a), plaintext2)   )
-    #plaintext2 = bytes(plaintext2, encoding='utf-8')
-    print(f"Decryption {plaintext2}")
+   
     return plaintext2
 
 if encrypting:
     #
     # Esper cipher
     #
-
+    
     # Get the key
-    keybytes = bytes(os.urandom(8))
-    keyrotate = keybytes[0] % 7 + 1
+    key = 'rORTrfA'
+    keyrotate = 2
     keyxor = []
-    key = ""
+    
     # Rotate the key
-    for i in range(1, 8):
-        keyxor.append(ord(string.ascii_letters[keybytes[i] % len(string.ascii_letters)]))
-        key = key + chr(keyxor[i-1])
+    for i in range(0,len(key)):
+        keyxor.append(ord(key[i]))
     print("The key is %s rotated by %d bits." % (key, keyrotate))
     print(f"keyxor {keyxor}")
     ciphertext = []
     for i in range(0, len(plaintext)):
         ciphertext.append(lrot(plaintext[i], keyrotate) ^ keyxor[i % len(keyxor)])
 
-    plaintext2 = bytes(decrypt(ciphertext,key,keyrotate)) 
-
+    
     with open(args.outfile, "wb") as output:
         output.write(bytes(ciphertext))
         output.close()
@@ -79,8 +75,12 @@ else:
     key = 'rORTrfA'
     keyrotate = 2
     plaintext2 = decrypt(ciphertext,key,keyrotate)    
+    plaintext2 = ''.join(map(lambda a : chr(a), plaintext2)   )    
+    print(f"Decryption {plaintext2}") 
+    plaintext2 = bytes(plaintext2,'utf-8')
     with open(args.outfile, "wb") as output:
-        output.write(bytes(plaintext2))
+        output.write(plaintext2)
         output.close()
+    
     exit(0)
 
